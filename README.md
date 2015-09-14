af75aff3b61e4f35b579a049dc7fe99a27c76a81
Markus Bergholz <markuman@gmail.com>
2015-09-14 22:19:03 +0200

    lol

--- README.md
+++ README.md
@@ -1,84 +1,9 @@
-# mex-lua
+# inline_lua
 ...the faster interpreter in the interpreter
 
-## MATLAB
-
-compiling directly in matlab
-
-    >> mex -llua -I /Users/markus/Downloads/lua-5.2.3/src/ CFLAGS='-Wall -Wextra -fPIC -std=c99 -O4 -pedantic -g'  lua.c
-
-## GNU OCTAVE
-
-compiling on linux with `tcc`, `gcc` or `clang` for octave or matlab _(just change the file extension)_
-
-    tcc -llua -I /path/to/mex.h/ -shared lua.c -o lua.mex
-    gcc -I /usr/include/octave-3.8.1/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
-    clang -I /usr/include/octave-3.8.1/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
-
-## Limitations
-
-1. Multidimension _(matrix)_ input is currently missing ...see Issue 1
-2. The order of returns can crash the stack ... see Issue 2
-3. Currently mex-lua needs at least one output ... see Issue 3
-4. Currently debug outputs are hardcoded
-
-## Usage
-
-create you lua function _(make sure filename and function name are the same. e.g. sum() and sum.lua)_
-
-    function sum (param1, param2)
-        return = param1 + param2
-    end
-
-call lua script from Matlab/GNU Octave
-
-    octave:1> ret = lua('sum', pi, e)
-    well done sum in sum.lua
-    lua stack top 1
-    0 is number, lua stack top 1
-    all done, lua stack top 0
-    ret =     5.85987448204884
-
-
-### BENCHMARKING
-
-You can easily use it for [sumdata](https://github.com/markuman/sumdata). This will bring the speed of lua _(< 5 sec)_ into GNU Octave _(> 180 sec)_ and Matlab e.g. without touching C/C++.
-
-#### octave benchmark
-
-    octave:1> tic, sumdata, toc
-    Original size 117261679.920000kb
-    Compressed size 96727557.137000kb
-    Elapsed time is 182.445 seconds.
-
-#### mex-lua benchmark
-
-    function sumdata(file)
-      local o, ot, c, ct, l = 0, 0, 0, 0, 1
-
-      for line in io.lines(file) do
-        for n in string.gmatch(line, "%S+") do
-          if l == 5 then
-            ot = n
-          elseif l == 6 then
-            ct = n
-            break
-          end
-          l = l + 1
-        end
-        l, o, c = 1, o + ot, c + ct
-      end
-      return (string.format("Original Size: %.0f kb\nCompressed Size: %.0f kb\n",o/1000,c/1000))
-    end
-
-    octave:2> tic, ret = lua('sumdata', 'data.log'), toc
-    well done sumdata in sumdata.lua
-    lua stack top 1
-    0 is string, lua stack top 1
-    all done, lua stack top 0
-    ret = Original Size: 117261680 kb
-    Compressed Size: 96727557 kb
-
-    Elapsed time is 4.61084 seconds.
-
-
+    octave:11> tic, a = lua('a = {} a[1] = 1 for n = 1,1000 do a[1] = a[1] + 1 end return a'){:}, toc
+    a =                 1001
+    Elapsed time is 0.000264883 seconds.
+    octave:12> tic, a = 1; for n = 1:1000, a = a + 1; end, a, toc
+    a =                 1001
+    Elapsed time is 0.00235295 seconds.
--- lua.c
+++ lua.c
@@ -2,12 +2,12 @@
 
 /*
  compiling directly in matlab
- mex -llua -I/Users/markus/Downloads/lua-5.2.3/src/ CFLAGS='-Wall -Wextra -fPIC -std=c99 -O4 -pedantic -g'  lua.c
+ mex -llua -I/Users/markus/Downloads/lua-5.3.1/src/ CFLAGS='-Wall -Wextra -fPIC -std=c99 -O4 -pedantic -g'  lua.c
 
  compiling on linux with tcc, gcc or clang for octave or matlab (just change the file extension
  tcc -llua -I /path/to/mex.h -shared lua.c -o lua.mex
- gcc -I/usr/include/octave-3.8.1/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
- clang -I/usr/include/octave-3.8.1/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
+ gcc -I/usr/include/octave-4.0.0/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
+ clang -I/usr/include/octave-4.0.0/octave/ -lm -llua -shared -fPIC -std=c99 -O4 lua.c -o lua.mex
 
  */
 
@@ -32,175 +32,129 @@
 // Matlab specific includes
 #include "mex.h"
 
+//#define DEBUG
+
+int top;
+mxArray *cell_array_ptr;
+
+
 void mexFunction (int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[])
 {
 
   // --- input checks
   // more than one
-  if(nrhs < 1) {
+  if(nrhs != 1) {
     mexErrMsgIdAndTxt( "MATLAB:lua:invalidNumInputs",
-                      "One or more inputs are required.");
+                      "One input is required.");
     // first input have to be a string which calls the lua file
     // furthermore: filename = function name
-  } else if ( nlhs < 1) {
-    mexErrMsgIdAndTxt("MATLAB:lua:nrhs", "You have to define outputs!");
+  } else if ( nlhs != 1) {
+    mexErrMsgIdAndTxt("MATLAB:lua:nlhs", "You have to define outputs!");
   } else if ( !mxIsChar(prhs[0])) {
-    mexErrMsgIdAndTxt("MATLAB:lua:nrhs", "First Input must be a string.");
+    mexErrMsgIdAndTxt("MATLAB:lua:nrhs", "Input must be a string.");
   } else {
 
-    // get string from nrhs
-    // while funcname == filename + .lua extension
-    char *luafunc, *luafile;
-    luafunc = (char *) mxCalloc(mxGetN(prhs[0])+1, sizeof(char));
-    luafile = (char *) mxCalloc(mxGetN(prhs[0])+5, sizeof(char));
-    mxGetString(prhs[0], luafunc, mxGetN(prhs[0])+1);
-    mxGetString(prhs[0], luafile, mxGetN(prhs[0])+1);
-    char file_extension[] = ".lua";
-    strcat(luafile, file_extension);
-    mexPrintf("well done %s in %s\n", luafunc, luafile);
+    char *luacode;
+    luacode = (char *) mxCalloc(mxGetN(prhs[0])+1, sizeof(char));   
+    mxGetString(prhs[0], luacode, mxGetN(prhs[0])+1);
 
     // try to open lua stack
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    // Tell Lua to load & run the file sample_script.lua
-    luaL_dofile(L, luafile);
-    // Push function to the Lua stack
-    lua_getglobal(L, luafunc);
-    // Checks if top of the Lua stack is a function.
-    lua_isfunction(L, -1);
+    // try to run the luacode
+    luaL_dostring(L, luacode);
+
 
     double *data;
     int columns, rows, len;
     char *string;
-    // put all inputs to lua stack
-    for(int in = 1; in < nrhs ; in++){
-      // is double array?
-      if (mxIsDouble(prhs[in])){
-
-        data = mxGetPr (prhs[in]);
-
-        // check dimension
-        columns = mxGetN(prhs[in]);
-        rows = mxGetM(prhs[in]);
-
-        // if one double, push one number to stack
-        if (columns == 1 && rows == 1){
-          if ( mxIsNumeric(prhs[in])) {
-            lua_pushnumber(L, data[0]);
-          }
-        } else {
-          // TODO, create/set table
-          mexErrMsgIdAndTxt("MATLAB:lua:nrhs", "Multidimension Matrix is not supported atm\n.");
-
-        }
-        // is string?
-      } else if (mxIsChar(prhs[in])){
-
-        string = (char *) mxCalloc(mxGetN(prhs[in])+1, sizeof(char));
-        mxGetString(prhs[in], string, mxGetN(prhs[in])+1);
-
-        lua_pushstring(L, string);
-      }
-
-    }
-
-
-    // Perform the function call (nrhs-1, nlhs) */
-    if (lua_pcall(L, nrhs-1, nlhs, 0) != 0) {
-      mexErrMsgIdAndTxt("MATLAB:lua:nrhs", "Failed to load lua file\n.");
-    }
-
-    int top = lua_gettop(L);
+  
+    #ifdef DEBUG
+    top = lua_gettop(L);
     mexPrintf("lua stack top %d\n", top);
+    #endif
 
-    // Let's retrieve the function result
-    // Check if the top of the Lua Stack has a numeric value
-    double *mexoutput;
-    double luaout;
-    const char* luastring;
-
-    for(int n=0; n<nlhs ; n++){
-
-      // is table
-      if (lua_istable(L, -1)) {
-        top = lua_gettop(L);
-        mexPrintf("%d is table, lua stack top %d\n", n, top);
+    if (lua_istable(L, 1)) {
+        #ifdef DEBUG
+        printf("top is table\n");
+        #endif
+        
+        
         int elements = 0;
-        rows = luaL_len(L, 1);
+        lua_len(L, 1);
+        rows = lua_tonumber(L, -1);
+        lua_pop(L, 1);
         lua_rawgeti(L, 1, 1);
         if (lua_istable(L, -1)) {
-          // a = {{1, 3}, {7, 0}}
-          columns = luaL_len(L, -1);
-          plhs[n] = mxCreateNumericMatrix(rows, columns, mxDOUBLE_CLASS, mxREAL);
-          mexoutput = mxGetPr(plhs[n]);
-
-          for (int r = 1; r <= rows; ++r) {
-
-            if ( r > 1 ){
-              lua_rawgeti(L, 1, r);
-              columns = luaL_len(L, -1);
-            }
-
-            for (int c = 1; c <= columns; ++c) {
-              lua_rawgeti(L, -1, c);
-              mexoutput[elements] = lua_tonumber(L, -1);
-              lua_pop(L, 1);
-              ++elements;
-            }
+            // a = {{1, 3}, {7, 0}}
+            #ifdef DEBUG
+            printf("top is table again\n");
+            #endif
+            lua_len(L, -1);
+            int columns = lua_tonumber(L, -1);
             lua_pop(L, 1);
-          }
-          lua_remove(L, 1);
+            #ifdef DEBUG
+            printf("rows %d, cols %d\n", rows, columns);
+            #endif
+            cell_array_ptr = mxCreateCellMatrix(rows,columns);                      
+            
+            for (int r = 1; r <= rows; ++r) {
+                
+                if ( r > 1 ){
+                    lua_rawgeti(L, 1, r);
+                    lua_len(L, -1);
+                    columns = lua_tonumber(L, -1);
+                    lua_pop(L, 1);
+                }
+                
+                for (int c = 1; c <= columns; ++c) {
+                    lua_rawgeti(L, -1, c);
+                    // column major order
+                    #ifdef DEBUG                    
+                    printf("index %d, row %d, col %d, nbrows %d\n", ( (c - 1) * rows + (r - 1) + 1), r, c, rows); 
+                    #endif
+                    if (lua_isnumber(L, -1)) {
+                        mxSetCell(cell_array_ptr,( (c - 1) * rows + (r - 1) + 1) - 1, mxCreateDoubleScalar(lua_tonumber(L, -1)));
+                    } else {
+                        mxSetCell(cell_array_ptr,( (c - 1) * rows + (r - 1) + 1) - 1, mxCreateString(lua_tostring(L, -1)));
+                    }
+                    lua_pop(L, 1);
+                    ++elements;
+                }
+                lua_pop(L, 1);
+            }
+            lua_remove(L, 1);
         } else {
-          // a = {1, 3, 7, 0}
-          top = lua_gettop(L);
-          mexPrintf("%d is vector, lua stack top %d\n", n, top);
-          plhs[n] = mxCreateNumericMatrix(1, rows, mxDOUBLE_CLASS, mxREAL);
-          mexoutput = mxGetPr(plhs[n]);
-          lua_pop(L, 1);
-          for (int v = 1; v <= rows; ++v) {
-            lua_rawgeti(L, -1, v);
-            mexoutput[v-1] = lua_tonumber(L, -1);
+            // a = {1, 3, 7, 0}
+            cell_array_ptr = mxCreateCellMatrix(1,rows);
+            #ifdef DEBUG
+            printf("rows %d\n", rows);
+            #endif
             lua_pop(L, 1);
-          }
-          top = lua_gettop(L);
-          mexPrintf("after vec %d lua stack top %d\n", n, top);
-          lua_remove(L, 1);
-          top = lua_gettop(L);
-          mexPrintf("after remove %d lua stack top %d\n", n, top);
+            lua_pushnil(L);
+            for (int v = 1; v <= rows; ++v) {
+                lua_next(L, -2);
+                if (lua_isnumber(L, -1)) {
+                    mxSetCell(cell_array_ptr, v - 1, mxCreateDoubleScalar(lua_tonumber(L, -1)));
+                } else {
+                    mxSetCell(cell_array_ptr, v - 1, mxCreateString(lua_tostring(L, -1)));
+                }
+                lua_pop(L, 1);
+            }
+            
+            lua_remove(L, 1);
+           
         }
+        plhs[0] = cell_array_ptr;
 
-      } else if (lua_isnumber(L, -1)) {
-
-        top = lua_gettop(L);
-        mexPrintf("%d is number, lua stack top %d\n", n, top);
-        luaout = lua_tonumber(L, -1); // retrieve the result
-        lua_pop(L, 1); // Pop retrieved value from the Lua stack
-        plhs[n] = mxCreateNumericMatrix(1, 1, mxDOUBLE_CLASS, mxREAL);
-        mexoutput = mxGetPr(plhs[n]);
-        mexoutput[0] = luaout;
-
-      } else if (lua_isstring(L, -1)) {
-        top = lua_gettop(L);
-        mexPrintf("%d is string, lua stack top %d\n", n, top);
-        luastring = lua_tostring(L, -1);
-        plhs[n] = mxCreateString(luastring);
-        lua_pop(L, 1);
-      } else {
-
-        mexPrintf("%d is unkown\n", n);
-
-      }
-
-
+    } else {
+        mexErrMsgIdAndTxt("MATLAB:lua:nlhs", "return always a table!");
     }
-
-    top = lua_gettop(L);
-    mexPrintf("all done, lua stack top %d\n", top);
+        
+        
     lua_close(L);
     return;
-
-    //mexPrintf("well done %s in %s\n", luafunc, luafile);
 
   }
 }
